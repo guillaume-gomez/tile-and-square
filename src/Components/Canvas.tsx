1382c8d477dd37a20339db23b923a5211770d190
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-11-23 00:26:20 +0100

    update colors as variable in canvas

--- src/Components/Canvas.tsx
+++ src/Components/Canvas.tsx
@@ -4,13 +4,26 @@ import useAnimationFrame from "../CustomHooks/useAnimationFrame";
 import { sample } from "lodash";
 import { TileData, strategyType, ExternalActionInterface } from "../interfaces";
 
-import tile_zero from "../Tiles/tile_0.png";
-import tile_one from "../Tiles/tile_1.png";
-import tile_second from "../Tiles/tile_2.png";
-import tile_third from "../Tiles/tile_3.png";
+import zero from "../Tiles/tile_0.png";
+import one from "../Tiles/tile_1.png";
+import second from "../Tiles/tile_2.png";
+import third from "../Tiles/tile_3.png";
+import four from "../Tiles/tile_4.png";
+import five from "../Tiles/tile_5.png";
+import six from "../Tiles/tile_6.png";
+import seven from "../Tiles/tile_7.png";
+import eight from "../Tiles/tile_8.png";
+import nine from "../Tiles/tile_9.png";
+import ten from "../Tiles/tile_10.png";
+import eleven from "../Tiles/tile_11.png";
+import twelve from "../Tiles/tile_12.png";
+import thirteen from "../Tiles/tile_13.png";
+import forteen from "../Tiles/tile_14.png";
+import fifteen from "../Tiles/tile_15.png";
+import sixteen from "../Tiles/tile_16.png";
+import seventeen from "../Tiles/tile_17.png";
 
-
-const TILES = [tile_zero, tile_one, tile_second, tile_third];
+const TILES = [zero, one, second, third, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, forteen, fifteen, sixteen, seventeen];
 
 
 interface EngineInterface {
@@ -57,15 +70,24 @@ const Canvas = forwardRef<ExternalActionInterface, CanvasInterface>(({speed, bac
       },
 
       pause() {
-        stop();
+          stop();
       },
 
       resetPosition() {
         resetPosition();
+        if(canvasRef && canvasRef.current) {
+          const context = canvasRef.current.getContext("2d");
+          renderArtwork(context);
+        }
       },
 
       resetAll() {
-        generateArtwork();
+        generateArtwork().then(() => {
+          if(canvasRef && canvasRef.current) {
+            const context = canvasRef.current.getContext("2d");
+            renderArtwork(context);
+          }
+        });
       }
     }));
 
@@ -95,13 +117,13 @@ const Canvas = forwardRef<ExternalActionInterface, CanvasInterface>(({speed, bac
     }
   }
 
-  function generateArtwork() {
+  async function generateArtwork() : Promise<void> {
     const { width, height } = canvasRef.current!;
     const widthTile = width / nbTilesWidth;
     const heightTile = height / nbTilesWidth;
 
     const tiles : TileData[] = [];
-    const promises = [];
+    const promises : Promise<TileData>[] = [];
 
     const strategyName = sample(["horizontal","vertical", "southEast", "northEast" , "southWest" , "northWest"]) as strategyType;
 
@@ -110,7 +132,7 @@ const Canvas = forwardRef<ExternalActionInterface, CanvasInterface>(({speed, bac
 
         const image = new Image();
         image.src = pickImage();
-        const promise = new Promise((resolve, reject) => { 
+        const promise = new Promise<TileData>((resolve, reject) => { 
           image.onload = () => {
             const [vx, vy] = computeVelocityByStrategy(strategyName);
             resolve({x, y , vx, vy, width: widthTile, height: heightTile, image });
@@ -120,10 +142,13 @@ const Canvas = forwardRef<ExternalActionInterface, CanvasInterface>(({speed, bac
       }
     }
 
-    Promise.all(promises)
-    .then(
-      (tiles) => {
-        tilesRef.current = tiles as TileData[];
+    return new Promise<void>((resolve) => {
+      Promise.all(promises)
+      .then(
+        (tiles) => {
+          tilesRef.current = tiles as TileData[];
+          resolve();
+      });
     });
   }
 
@@ -141,7 +166,7 @@ const Canvas = forwardRef<ExternalActionInterface, CanvasInterface>(({speed, bac
     for(let x = 0; x < width; x += widthTile) {
       for(let y = 0; y < height; y += heightTile) {
         tiles.push({...tilesRef.current[index], x, y });
-        index += index;
+        index = index +1;
       }
     }
     tilesRef.current = tiles;
@@ -167,7 +192,6 @@ const Canvas = forwardRef<ExternalActionInterface, CanvasInterface>(({speed, bac
     let newY = y + (vy * (speed/100) * deltaTime );
     let newVx = vx;
     let newVy = vy;
-    let newStrategy = false;
 
     const widthCanvas = canvasRef!.current!.width;
     const heightCanvas = canvasRef!.current!.height;
@@ -211,8 +235,9 @@ const Canvas = forwardRef<ExternalActionInterface, CanvasInterface>(({speed, bac
     })
   }
 
+  const colorClass = `bg-${backgroundColorClass}`
   return (
-    <canvas ref={canvasRef} className={`border-2 rounded-lg bg-${backgroundColorClass}`}>
+    <canvas ref={canvasRef} className={`border-2 rounded-lg ${colorClass}`}>
     </canvas>
   );
 });
