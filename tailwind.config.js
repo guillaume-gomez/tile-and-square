74ed028803af92393f7734809fbae79bef2edaad
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-11-23 00:34:27 +0100

    whitelist colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,21 @@
+const COLORS = ['gray', 'red','yellow', 'green', 'blue', 'indigo', 'purple', 'pink'];
+const VARIANTS = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+const whitelist = []
+COLORS.forEach((color) => {
+  VARIANTS.forEach((variant) => {
+    whitelist.push(`bg-${color}-${variant}`)
+  })
+})
+
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    enable: process.env.NODE_ENV === "production",
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    options: {
+      whitelist
+    }
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
