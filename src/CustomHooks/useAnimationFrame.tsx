81aa4c3aa89c309f007b78aef964e0e2d8d4dece
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-11-24 09:44:59 +0100

    use null to explicite logic

--- src/CustomHooks/useAnimationFrame.tsx
+++ src/CustomHooks/useAnimationFrame.tsx
@@ -3,14 +3,14 @@ import { useRef, useEffect } from "react";
 export default function useAnimationFrame(callback: Function) {
   // Use useRef for mutable variables that we want to persist
   // without triggering a re-render on their change
-  const requestRef = useRef(0);
-  const previousTimeRef = useRef(0);
+  const requestRef = useRef<number | null>(null);
+  const previousTimeRef = useRef<number| null>(null);
   /**
    * The callback function is automatically passed a timestamp indicating
    * the precise time requestAnimationFrame() was called.
    */
   function animate(time: number) {
-    if (previousTimeRef.current !== undefined) {
+    if (previousTimeRef.current !== null) {
       const deltaTime = time - previousTimeRef.current;
       callback(deltaTime);
     }
@@ -21,12 +21,13 @@ export default function useAnimationFrame(callback: Function) {
   function stop() {
     if(requestRef.current) {
       cancelAnimationFrame(requestRef.current);
-      requestRef.current = 0;
+      requestRef.current = null;
+      previousTimeRef.current = null;
     }
   }
 
   function play() {
-    if(requestRef.current === 0) {
+    if(requestRef.current === null) {
       requestRef.current = requestAnimationFrame(animate);
     }
   }
