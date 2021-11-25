a7d38403c6d92d828b086b9eda8ca862e3f0ed92
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-11-25 12:13:36 +0100

    whitelist colors classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,6 @@ COLORS.forEach((color) => {
 
 module.exports = {
   purge: {
-    enable: process.env.NODE_ENV === "production",
     content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     safelist
   },
