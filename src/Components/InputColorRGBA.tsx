8429461f661b1b66104641a92c645dc6e914dd9b
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-12-20 23:08:55 +0100

    create 2 customs input colors

---
+++ src/Components/InputColorRGBA.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import InputColor from "./InputColor";
+import SliderWithLabel from "./SliderWithLabel";
+
+interface InputColorRGBAInterface {
+  onChange: (rgba: string) => void;
+  color: string;
+}
+
+function InputColorRGBA({ onChange, color } : InputColorRGBAInterface) {
+  const [opacity, setOpacity] = useState<number>(255);
+
+  function computeColor(color: string, opacity: number) : string {
+    let newColor = color;
+    if(color.length > 7) {
+      newColor = color.substring(0, 7);
+    }
+    return `${newColor}${opacity.toString(16)}`;
+  }
+
+  function changeOpacity(opacity: number) {
+    onChange(computeColor(color, opacity));
+    setOpacity(opacity);
+  }
+  return (
+    <div className="flex flex-col">
+      <InputColor onChange={(color) => onChange(color) } color={computeColor(color, opacity)} />
+      <SliderWithLabel label="Opacity" value={opacity} max={255} onChange={(value) => changeOpacity(parseInt(value))} />
+    </div>
+  );
+}
+
+export default InputColorRGBA;
+
+
