ffa1a13d85add205ecaf8aaf60b83ca36e866336
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-12-29 19:38:47 +0100

    add new route

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,17 @@ import {
 } from "react-router-dom";
 import Artwork from "./views/Artwork";
 import DiyForm from "./views/DiyForm";
+import Layout from "./views/Layout";
 
 function App() {
   return (
   <BrowserRouter>
     <Routes>
+      <Route path="tile-and-square" element={<Layout />}>
+        <Route path="diy" element={<DiyForm />} />
+        <Route index element={<Artwork />} />
+      </Route>
       <Route path="*" element={<Artwork />} />
-      <Route path="/diy" element={<DiyForm />} />
-      
     </Routes>
   </BrowserRouter>
   )
